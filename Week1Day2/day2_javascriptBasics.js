fa595a94a37beacb1c9d3f6c1db1b7108bfae3af
srijita890 <cool.srijita123@gmail.com>
2026-08-05 18:26:51 +0530

    Add files via upload
    Week1 Day2

---
+++ Week1Day2/day2_javascriptBasics.js
@@ -0,0 +1,13 @@
+var firstName = 'Srijita'
+var companyName= 'Test'
+var mobileNumber= 8971517924
+var isAutomation= false
+var hasPlaywright
+
+
+console.log('First name is ' + firstName + '  And Type is' + ' '+ typeof firstName)
+console.log('Company Name is ' + companyName + '  And Type is' + ' '+ typeof companyName)
+console.log('Mobile Number is ' + mobileNumber + '  And Type is' + ' '+ typeof mobileNumber)
+console.log('Is Automation is ' + isAutomation + '  And Type is' + ' '+ typeof isAutomation)
+console.log('Has playwright is ' + hasPlaywright + ' And Type is' + ' '+ typeof hasPlaywright)
+
