e1fe0d880a46caf66b7dc365d57ff20d1021e9ad
srijita890 <cool.srijita123@gmail.com>
2026-08-08 12:03:51 +0530

    Add files via upload

---
+++ Week1Day4/isOddOrEven.js
@@ -0,0 +1,12 @@
+function isOddOrEven(number){
+    if((number%2)== 0)
+    {
+        console.log ('Given number is even ', number)
+    }
+    else{
+        console.log ('Given number is odd ', number)
+    }
+}
+
+let number = 26
+isOddOrEven(number)
